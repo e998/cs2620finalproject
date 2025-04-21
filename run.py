beb4e7523776f049542eca01508b8e3f635e8bd7
Sammi <sammizhu@college.harvard.edu>
2025-04-21 13:55:51 -0400

    new IP

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+import os
 from app import create_app, db, socketio
 
 load_dotenv()
@@ -7,4 +8,4 @@ app = create_app()
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, host=os.environ.get('HOST', '10.250.20.28'))
